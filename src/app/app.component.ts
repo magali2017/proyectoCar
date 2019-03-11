b3849f9050a3b94485034d7112e941e1aeaecd63
Magali Arana <aranamagali4@gmail.com>
2019-03-11 01:31:14 -0400

    agregando angular material

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyectoCar1';
+  selectedValue: string = "";
+  items = [
+    { value: "0", view: "zero" },
+    { value: "1", view: "one" },
+    { value: "2", view: "Two" }
+  ];
+}
+
+
+ 
